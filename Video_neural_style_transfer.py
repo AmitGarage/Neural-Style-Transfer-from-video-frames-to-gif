90371d80f61cb1ba998cccd1793e94468b292a82
Amit Singh <amitsingh9994@gmail.com>
2021-05-04 13:05:06 +0530

    Update Video_neural_style_transfer.py

--- Video_neural_style_transfer.py
+++ Video_neural_style_transfer.py
@@ -1,9 +1,6 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
-
-
 import cv2
 import os
 import shutil
@@ -22,51 +19,33 @@ import torchvision.models as models
 import copy
 
 
-# In[2]:
-
-
+# Uploading Video to desired path
 video = cv2.VideoCapture('/notebooks/Notebooks_2019/Style_Data/Test4.mp4')
 
+#os.path.exists('/notebooks/Notebooks_2019/Style_Data/Images_from_video')
 
-# In[3]:
-
-
-os.path.exists('/notebooks/Notebooks_2019/Style_Data/Images_from_video')
-
-
-# In[4]:
-
-
+# Deleting folder ( Images_from_video ) along with data present inside
 try :
     if os.path.exists('/notebooks/Notebooks_2019/Style_Data/Images_from_video') :
         shutil.rmtree('/notebooks/Notebooks_2019/Style_Data/Images_from_video')
 except OSError:
     print('Error in deleting directory')
 
-
-# In[5]:
-
-
+# Deleting folder ( Video_from_images ) along with data present inside
 try :
     if os.path.exists('/notebooks/Notebooks_2019/Style_Data/Video_from_images') :
         shutil.rmtree('/notebooks/Notebooks_2019/Style_Data/Video_from_images')
 except OSError:
     print('Error in deleting directory')
 
-
-# In[6]:
-
-
+# Creating folder ( Images_from_video )
 try :
     if not os.path.exists('/notebooks/Notebooks_2019/Style_Data/Images_from_video') :
         os.makedirs('/notebooks/Notebooks_2019/Style_Data/Images_from_video')
 except OSError:
     print('Error in creating directory')
 
-
-# In[7]:
-
-
+# Creating folder ( Video_from_images )
 try :
     if not os.path.exists('/notebooks/Notebooks_2019/Style_Data/Video_from_images') :
         os.makedirs('/notebooks/Notebooks_2019/Style_Data/Video_from_images')
@@ -74,34 +53,42 @@ except OSError:
     print('Error in creating directory')
 
 
-# In[8]:
-
-
+# Function to extract image frames from video uploaded
 def getFrame(sec) :
+    # Seting current position of the video file in milliseconds.
     video.set(cv2.CAP_PROP_POS_MSEC, sec*1000)
+    # Check if frame is read correctly
     video_length , image_frame = video.read()
+    # Check if video_length is no more left to extract image frame
     if video_length :
+        # Images name with which image frames will be saved
         image_name = '/notebooks/Notebooks_2019/Style_Data/Images_from_video/image_'+str(frame_count)+'.jpg'
-        print(image_name+' created')
+        #print(image_name+' created')
+        # Saving image files
         cv2.imwrite(image_name,image_frame)
+    # returning whether video length is left or not to extract image frame
     return video_length
 
+# Initalizing frame count to be used in naming image files
 frame_count = 1
+# Initalizing sec from where images will be extracted in video
 sec = 0
+# Initalizing frame rate ( frames per second ) with which images will be extracted
 frame_Rate = 0.25
+# Initalizing and checking if video length left after 1 iteriation
 frame_check = getFrame(sec)
 while frame_check :
+    # Increasing frame count
     frame_count += 1
+    # Increasing sec to sec + frame_Rate to move image extractin video forward 
     sec = sec + frame_Rate
     sec = round(sec,2)
     frame_check = getFrame(sec)
     
+# Releasing video capture
 video.release()
+# Closing all frames
 cv2.destroyAllWindows()
-
-
-# In[9]:
-
 
 #frame_count = 0
 #while(True) :
@@ -117,10 +104,6 @@ cv2.destroyAllWindows()
 #video.release()
 #cv2.destroyAllWindows()
 
-
-# In[10]:
-
-
 #Test_image = Image.open('/notebooks/Notebooks_2019/Style_Data/Images_from_video/image_1.jpg')
 #print(Test_image.size)
 #(height,width) = Test_image.size
@@ -129,16 +112,15 @@ cv2.destroyAllWindows()
 #New_width = width*height_percentage
 #print(New_height,int(New_width))
 
-
-# In[11]:
-
-
+# Variable to check which device ( CPU or GPU - if available ) to use for processing
 device_to_use = ('cuda' if torch.cuda.is_available() else 'cpu')
 
 #print(device_to_use)
 
+# Initalizing image size to convert all extracted images
 imagesize = (512,512)
 
+# Transformations to be applied to images before any processing
 image_transformation = transforms.Compose([transforms.Resize(imagesize),transforms.ToTensor()])
 
 #image = Image.open('/notebooks/Notebooks_2019/Style_Data/Test1.jpeg')
@@ -153,9 +135,13 @@ image_transformation = transforms.Compose([transforms.Resize(imagesize),transfor
 #image = image.to(device_to_use, torch.float)
 #print(image)
 
+# Function to load images from defined path and load to device ( CPU / GPU )
 def image_loader(path_to_image):
+    # Image opening
     image = Image.open(path_to_image)
+    # Transforming image
     image = image_transformation(image).unsqueeze(0)
+    # Returing image after loading to device
     return image.to(device_to_use, torch.float)
 
 #print(Actual_image.shape)
@@ -167,51 +153,72 @@ def image_loader(path_to_image):
 #axis[0].imshow(unloader(Actual_image.cpu().clone().squeeze(0)))
 #axis[1].imshow(unloader(Style_image.cpu().clone().squeeze(0)))
 
+# Convert a tensor or an ndarray to PIL Image
 unloader = transforms.ToPILImage()
 
+# Function to display image
 def image_display(tensor , title = None) :
+    # Loading to CPU
     image = tensor.cpu().clone()
+    # Remove single-dimensional entries from the shape.
     image = image.squeeze(0)
+    # Converting to PIL Image
     image = unloader(image)
+    # Displaying Image
     plt.imshow(image)
     if title is not None :
+        # Setting image title
         plt.title(title)
 
+# Function to calculate weighted version of the content distance for an individual layer
 class PicContentLoss(nn.Module) :
     def __init__(self , target) :
         super(PicContentLoss , self).__init__()
+        # Gradient is no longer being computed using detach()
         self.target = target.detach()
         
     def forward(self , input) :
+        # calculating mean squared loss between input and target
         self.loss = F.mse_loss(input , self.target)
+        # Returns the layer’s input
         return input
 
+# Function to compute gram matrix used for calculating style loss
 def gram_matrix(input) :
     batch_size , no_of_channels , height , width = input.size()
+    # Resizing
     features_matrix = input.view(batch_size * no_of_channels , width * height)
+    # Calculating gram matrix by multiplying feature matrix with its tranpose
     gram_product = torch.mm(features_matrix , features_matrix.t())
+    # return normalized form of gram matrix to avoid any high value
     return gram_product.div(batch_size * no_of_channels * width * height)
 
+# Function to calculate weighted version of the style distance for an individual layer
 class PicStyleLoss(nn.Module) :
     def __init__(self , target_feature) :
         super(PicStyleLoss , self).__init__()
+        # Gradient is no longer being computed using detach()
         self.target = gram_matrix(target_feature).detach()
         
     def forward(self , input) :
         Gram = gram_matrix(input)
+        # calculating mean squared loss between gram matrix and target
         self.loss = F.mse_loss(Gram , self.target)
         return input
 
 #target = cnn(Actual_image).detach()
 #PicContentLoss(target).loss
 
+# Importing VGG19 along with pretrained weights and set to eval mode
 cnn = models.vgg19(pretrained=True).features.to(device_to_use).eval()
 #print(cnn)
 #for layer in cnn.children() : print(layer)
 
+# Imagenet pretrianed model mean and standard deviation 
 cnn_normalization_mean = torch.tensor([0.485, 0.456, 0.406]).to(device_to_use)
 cnn_normalization_std = torch.tensor([0.229, 0.224, 0.225]).to(device_to_use)
 
+# Function to normalize to input image
 class Normalization_input(nn.Module) :
     def __init__(self,mean,std) :
         super(Normalization_input, self).__init__()
@@ -221,18 +228,26 @@ class Normalization_input(nn.Module) :
     def forward(self , image_to_be_normalized) :
         return (image_to_be_normalized - self.mean) / self.std
 
+# Desired layers to compute style/content losses
 content_layers_default = ['conv_4']
 style_layers_default = ['conv_1','conv_2','conv_3','conv_4','conv_5']
 
+# Function to calulate loss as per layers
 def get_style_model_and_losses(cnn ,normalization_mean ,normalization_std ,style_image ,
                                content_image ,content_layers = content_layers_default ,
                                style_layers = style_layers_default) :
+    # Copy model
     cnn = copy.deepcopy(cnn)
+    # Normalize input
     normalization_module = Normalization_input(normalization_mean,normalization_std).to(device_to_use)
+    # Initalize both losses list to store
     content_losses = []
     style_losses = []
+    # Initalize model and add layers in sequential order
     model = nn.Sequential(normalization_module)
+    # Initalize layer number
     conv_layer = 0
+    # Loop to add layer and layer name to model
     for layer in cnn.children() :
         if isinstance(layer ,nn.Conv2d) :
             conv_layer += 1
@@ -251,6 +266,7 @@ def get_style_model_and_losses(cnn ,normalization_mean ,normalization_std ,style
         #print(model)
         
         if layer_name in content_layers :
+            # Add content loss 
             target = model(content_image).detach()
             #print(model(content_image).shape)
             #print(target.shape)
@@ -260,6 +276,7 @@ def get_style_model_and_losses(cnn ,normalization_mean ,normalization_std ,style
             content_losses.append(Pic_ContentLoss)
         
         if layer_name in style_layers :
+            # Add style loss
             target_feature = model(style_image).detach()
             #print(target_feature.shape)
             Pic_StyleLoss = PicStyleLoss(target_feature)
@@ -269,21 +286,25 @@ def get_style_model_and_losses(cnn ,normalization_mean ,normalization_std ,style
             #print(style_losses)
          
         #print(conv_layer)
+    # Loop from reverse to count no of layer in model excluding any flatten layer
     for conv_layer in range(len(model)-1,-1,-1) :
         if isinstance(model[conv_layer], PicContentLoss) or isinstance(model[conv_layer], PicStyleLoss) :
             break
     
     #print(conv_layer)  
     #print(model[conv_layer])
+    # Final model over which training will be done
     model = model[:(conv_layer+1)]
     #print(model)
         
     return model , style_losses , content_losses
 
+# Function defining optimizer
 def input_optimizer (input_image) :
     optimizer = optim.LBFGS([input_image.requires_grad_()])
     return optimizer
 
+# Function to execute neural transfer over image
 def neural_transfer_execute (cnn ,normalization_mean ,normalization_std ,style_image ,content_image ,
                              input_image, num_of_steps = 150, style_weight = 100000, content_weight = 1) :
     
@@ -298,7 +319,9 @@ def neural_transfer_execute (cnn ,normalization_mean ,normalization_std ,style_i
     run = [0]
     while run[0] <= num_of_steps :
         def input_image_update() :
+            # Clamp all elements in input into the range [ min, max ] ; y = min(max(x,min_value),max_value)
             input_image.data.clamp_(0,1)
+            # Sets gradients of all model parameters to zero.
             optimizer.zero_grad()
             model(input_image)
             style_score = 0
@@ -317,6 +340,7 @@ def neural_transfer_execute (cnn ,normalization_mean ,normalization_std ,style_i
             content_score *= content_weight
             
             loss = style_score + content_score
+            # Computes the derivative of the loss w.r.t. the parameters
             loss.backward()
             
             run[0] += 1
@@ -328,16 +352,14 @@ def neural_transfer_execute (cnn ,normalization_mean ,normalization_std ,style_i
                 
             return style_score + content_score
         
+        # Optimizer to take a step based on the gradients of the parameters
         optimizer.step(input_image_update)
         
     input_image.data.clamp_(0,1)
-    
+    # Return converted image
     return input_image
 
 #unloader(output.cpu().clone().squeeze(0)).save('/notebooks/Notebooks_2019/Style_Data/Output.jpg')
-
-
-# In[12]:
 
 
 path_modified_output_image = '/notebooks/Notebooks_2019/Style_Data/Video_from_images/'
@@ -345,6 +367,7 @@ path_input_image = '/notebooks/Notebooks_2019/Style_Data/Images_from_video/'
 
 Style_image = image_loader('/notebooks/Notebooks_2019/Style_Data/Style15.jpg')
 
+# Loop over all extracted images from video and convert them in new style
 image_files = [file for file in os.listdir(path_input_image) if isfile(join(path_input_image,file))]
 for x in range(len(image_files)) :
     input_image_filename = path_input_image+image_files[x]
@@ -354,6 +377,7 @@ for x in range(len(image_files)) :
     (height,width) = image_size_check.size
     New_height = 512
     height_percentage = New_height/height
+    # Calculating width as per height reduction percentage
     New_width = width*height_percentage
     print(height,width)
     print(New_height,int(New_width))
@@ -362,6 +386,7 @@ for x in range(len(image_files)) :
     input_image = Actual_image.clone()
     plt.figure()
     image_display(input_image,'Input Image')
+    # Calling model to convert image
     output_image = neural_transfer_execute (cnn ,cnn_normalization_mean ,cnn_normalization_std ,Style_image ,Actual_image , input_image)
     plt.figure()
     image_display(output_image,'Output Image')
@@ -369,12 +394,10 @@ for x in range(len(image_files)) :
     output_image_transformation = transforms.Compose([transforms.Resize((height,width))])
     #output_image = output_image_transformation(output_image)
     print(output_image.shape)
+    # Image save
     unloader(output_image).save(output_image_filename)
 
-
-# In[13]:
-
-
+# Converting all Images to GIF
 path_input_image = '/notebooks/Notebooks_2019/Style_Data/Video_from_images/'
 path_output_image = '/notebooks/Notebooks_2019/Style_Data/final_video.mp4'
 frame_Rate = 2
@@ -389,9 +412,8 @@ for x in range(len(image_files)) :
     image = cv2.imread(image_filename)
     h,w,layers = image.shape
     size=(w,h)
-    frame_array.append(image)
+    frame_array.append(image) 
 
-#print(len(frame_array))    
     
 output = cv2.VideoWriter(path_output_image,cv2.VideoWriter_fourcc(*'MJPG'),frame_Rate,size)
 for i in range(len(frame_array)) :
@@ -400,10 +422,3 @@ for i in range(len(frame_array)) :
 output.release()
 video.release()
 cv2.destroyAllWindows()
-
-
-# In[ ]:
-
-
-
-
